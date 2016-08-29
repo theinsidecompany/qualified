1b9a1bd4fb5acdcf9dc005d1196551a0f59055c0
jnySilva <jsilva@theinsidecompany.com>
2016-08-29 10:03:32 -0300

    Jonathan

--- app/controladores/controllerLote.js
+++ app/controladores/controllerLote.js
@@ -205,129 +205,127 @@ exports.LoteSellos = function(req, resp) {
 }
 
 
-//Metodo para añadir mensajes a la solicitud.
-//-------------------------------------------------------------------------------------------------------------------------------------//
-exports.actualizarLoteSolicitud = function(req, resp) {
-
-    var id_lote = req.body.id_lote;
-    var id_materiaPrima = req.body.materiaPrima;
-    var id_bodega = req.body.bodega;
-    var id_trader = req.body.trader;
-    var contenedor = req.body.contenedor;
-    var lote = req.body.lote;
-    var cantidad = req.body.cantidad;
-    var bultos = req.body.bultos;
-    var cliente = req.body.cliente;
-    var id_usuario = req.body.id_usuario;
-    var pais = req.body.paisTrader;
-    var tipoMuestreo = req.body.tipoMuestreo;
-    var estadoLab = req.body.estadoLab;
-
-    MateriaPrima.findOne({
-        id_materiaPrima: id_materiaPrima
-    }, function(err, materia) {
-
-        Bodega.findOne({
-            id_bodega: id_bodega
-        }, function(err, bodega) {
-
-            Trader.findOne({
-                id_trader: id_trader
-            }, function(err, trader) {
-
-                Pais.findOne({
-                    nombrePais: pais
-                }, function(err, pais) {
-
-                    TipoMuestreo.findOne({
-                        id_tipoMuestreo: tipoMuestreo
-                    }, function(err, muestreo) {
-
-                        var nuevoLote = {
-                            bodega: bodega,
-                            contenedor: contenedor,
-                            cantidad: cantidad,
-                            bultos: bultos,
-                            lote: lote,
-                            trader: trader,
-                            materiaPrima: materia,
-                            id_lote: id_lote,
-                            paisTrader: pais,
-                            tipoMuestreo: muestreo,
-                            estadoLab: 'vacio'
-                        };
-
-                        Solicitud.findOne({
-                            id_solicitud: req.params.id_solicitud
-                        }, function(err, solicitud) {
-
-                            var nuevosLotes = solicitud.lote;
-
-                            for (var i = 0; i < nuevosLotes.length; i++) {
-
-                                if (nuevosLotes[i].id_lote === id_lote) {
-
-                                    nuevosLotes.splice(i, 1);
-
-                                }
-
-                            }
-
-                            nuevosLotes.push(nuevoLote);
-
-                            var nuevaSolicitud = {
-                                id_solicitud: solicitud.id_solicitud,
-                                id_solicitud_cliente: solicitud.id_solicitud_cliente,
-                                id_orden_trabajo: solicitud.id_orden_trabajo,
-                                cliente: cliente,
-                                fecha_creacion: solicitud.fecha_creacion,
-                                fecha_muestreo: solicitud.fecha_muestreo,
-                                fecha_planificacion: solicitud.fecha_planificacion,
-                                estado: solicitud.estado,
-                                proceso: solicitud.proceso,
-                                lote: nuevosLotes,
-                                inspector: solicitud.inspector,
-                                mensaje: solicitud.mensaje
-                            };
-                            Solicitud.remove({
-                                id_solicitud: req.params.id_solicitud
-                            }, function(err, respuesta) {
-
-                            });
-
-                            Solicitud.create(nuevaSolicitud, function(err, respuesta) {
-
-                                Solicitud.find({
-                                    'cliente.id_usuario': id_usuario
-                                }, function(err, solicitudes) {
-                                    if (solicitudes === null) {
-                                        resp.send(null);
-                                    } else {
-                                        resp.send(solicitudes);
-
-                                    }
-                                }).sort({
-                                    id_solicitud: 1
-                                });
-
-                            });
-
-
-                        });
-
-                    })
-
-                });
-
-            });
-
-        });
-
-    });
-
-
-
-}
+// //Metodo para añadir mensajes a la solicitud.
+// //-------------------------------------------------------------------------------------------------------------------------------------//
+// exports.actualizarLoteSolicitud = function(req, resp) {
+//
+//     var id_lote = req.body.id_lote;
+//     var id_materiaPrima = req.body.materiaPrima;
+//     var id_bodega = req.body.bodega;
+//     var id_trader = req.body.trader;
+//     var contenedor = req.body.contenedor;
+//     var lote = req.body.lote;
+//     var cantidad = req.body.cantidad;
+//     var bultos = req.body.bultos;
+//     var cliente = req.body.cliente;
+//     var id_usuario = req.body.id_usuario;
+//     var pais = req.body.paisTrader;
+//     var tipoMuestreo = req.body.tipoMuestreo;
+//     var estadoLab = req.body.estadoLab;
+//
+//     MateriaPrima.findOne({
+//         id_materiaPrima: id_materiaPrima
+//     }, function(err, materia) {
+//
+//         Bodega.findOne({
+//             id_bodega: id_bodega
+//         }, function(err, bodega) {
+//
+//             Trader.findOne({
+//                 id_trader: id_trader
+//             }, function(err, trader) {
+//
+//                 Pais.findOne({
+//                     nombrePais: pais
+//                 }, function(err, pais) {
+//
+//                     TipoMuestreo.findOne({
+//                         id_tipoMuestreo: tipoMuestreo
+//                     }, function(err, muestreo) {
+//
+//                         var nuevoLote = {
+//                             bodega: bodega,
+//                             contenedor: contenedor,
+//                             cantidad: cantidad,
+//                             bultos: bultos,
+//                             lote: lote,
+//                             trader: trader,
+//                             materiaPrima: materia,
+//                             id_lote: id_lote,
+//                             paisTrader: pais,
+//                             tipoMuestreo: muestreo,
+//                             estadoLab: 'vacio'
+//                         };
+//
+//                         Solicitud.findOne({
+//                             id_solicitud: req.params.id_solicitud
+//                         }, function(err, solicitud) {
+//
+//                             var nuevosLotes = solicitud.lote;
+//
+//                             for (var i = 0; i < nuevosLotes.length; i++) {
+//
+//                                 if (nuevosLotes[i].id_lote === id_lote) {
+//
+//                                     nuevosLotes.splice(i, 1);
+//
+//                                 }
+//
+//                             }
+//
+//                             nuevosLotes.push(nuevoLote);
+//
+//                             var nuevaSolicitud = {
+//                                 id_solicitud: solicitud.id_solicitud,
+//                                 id_solicitud_cliente: solicitud.id_solicitud_cliente,
+//                                 id_orden_trabajo: solicitud.id_orden_trabajo,
+//                                 cliente: cliente,
+//                                 fecha_creacion: solicitud.fecha_creacion,
+//                                 fecha_muestreo: solicitud.fecha_muestreo,
+//                                 fecha_planificacion: solicitud.fecha_planificacion,
+//                                 estado: solicitud.estado,
+//                                 proceso: solicitud.proceso,
+//                                 lote: nuevosLotes,
+//                                 inspector: solicitud.inspector,
+//                                 mensaje: solicitud.mensaje
+//                             };
+//                             Solicitud.remove({
+//                                 id_solicitud: req.params.id_solicitud
+//                             }, function(err, respuesta) {
+//
+//                             });
+//
+//                             Solicitud.create(nuevaSolicitud, function(err, respuesta) {
+//
+//                                 Solicitud.find({
+//                                     'cliente.id_usuario': id_usuario
+//                                 }, function(err, solicitudes) {
+//                                     if (solicitudes === null) {
+//                                         resp.send(null);
+//                                     } else {
+//                                         resp.send(solicitudes);
+//
+//                                     }
+//                                 }).sort({
+//                                     id_solicitud: 1
+//                                 });
+//
+//                             });
+//
+//
+//                         });
+//
+//                     })
+//
+//                 });
+//
+//             });
+//
+//         });
+//
+//     });
+//
+// }
 
 exports.modificarLotes = function(req, resp) {
 
